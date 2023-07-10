33d3fc9196ef8ce34bdbbb19dfb1319101593a6b
Janek Korczynski <janekkorczynski@Janeks-MacBook-Pro.local>
2023-07-10 22:07:49 +0900

    config commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,27 +1,9 @@
-/**
- * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
- * for Docker builds.
- */
-
-await import("./src/env.mjs");
-
-
-/** @type {import("next").NextConfig} */
 const config = {
   reactStrictMode: true,
+  // Add your other configuration options here
 
-  /**
-   * If you have `experimental: { appDir: true }` set, then you must comment the below `i18n` config
-   * out.
-   *
-   * @see https://github.com/vercel/next.js/issues/41980
-   */
-  // i18n: {
-  //   locales: ["en"],
-  //   defaultLocale: "en",
-  // },
+  // Add this line to skip environment variable validation during the build process
+  skipEnvValidation: true,
 };
 
 export default config;
-
-
