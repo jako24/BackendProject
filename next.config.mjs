df4f75efa3ad4340928580da8e396706dd4fd265
Janek Korczynski <janekkorczynski@Janeks-MacBook-Pro.local>
2023-07-10 10:33:54 +0900

    New commit 4

--- next.config.mjs
+++ next.config.mjs
@@ -3,12 +3,8 @@
  * for Docker builds.
  */
 
-/**
- * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
- * for Docker builds.
- */
-
 await import("./src/env.mjs");
+
 
 /** @type {import("next").NextConfig} */
 const config = {
@@ -26,36 +22,6 @@ const config = {
   // },
 };
 
-module.exports = {
-  modularizeImports: {
-    'pages': {
-      transform: 'pages/{{ kebabCase member }}',
-    },
-  },
-};
-
 export default config;
-
-// await import("./src/env.mjs");
-
-
-// /** @type {import("next").NextConfig} */
-// const config = {
-//   reactStrictMode: true,
-
-//   /**
-//    * If you have `experimental: { appDir: true }` set, then you must comment the below `i18n` config
-//    * out.
-//    *
-//    * @see https://github.com/vercel/next.js/issues/41980
-//    */
-//   // i18n: {
-//   //   locales: ["en"],
-//   //   defaultLocale: "en",
-//   // },
-// };
-
-// export default config;
-
 
 
