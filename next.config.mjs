fab1ef738eeefaa46aad365c08930a6f72508d53
Janek Korczynski <janekkorczynski@Janeks-MacBook-Pro.local>
2023-07-10 23:39:18 +0900

    config commit 6

--- next.config.mjs
+++ next.config.mjs
@@ -27,10 +27,12 @@ await import("./src/env.mjs");
 const config = {
   reactStrictMode: true,
 
-  i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
-  },
+  experimental: { appDir: true},
+
+  // i18n: {
+  //   locales: ["en"],
+  //   defaultLocale: "en",
+  // },
 };
 
 export default config;
